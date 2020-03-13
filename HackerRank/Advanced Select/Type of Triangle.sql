18dd9caf013195e56c320f61e10c56145ef85579
Abu Bakar Siddique <jabihad@gmail.com>
2020-03-13 16:54:22 +0600

    Update Type of Triangle.sql

--- HackerRank/Advanced Select/Type of Triangle.sql
+++ HackerRank/Advanced Select/Type of Triangle.sql
@@ -1,6 +1,6 @@
 SELECT
   CASE
-    WHEN (A+B <= C OR  B+C <= A OR C+A <= B) THEN "Not A Triangle"
+    WHEN A+B <= C OR  B+C <= A OR C+A <= B THEN "Not A Triangle"
     WHEN A = B AND B = C THEN "Equilateral"
     WHEN A = B OR B = C OR c = A THEN "Isosceles"
     WHEN A != B AND B != C THEN "Scalene"
