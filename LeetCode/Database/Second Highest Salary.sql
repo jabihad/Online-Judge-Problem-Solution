dcecd87b819cee0a23f48851e8aa6d843c031d52
Abu Bakar Siddique <jabihad@gmail.com>
2020-03-17 13:56:51 +0600

    Update Second Highest Salary.sql

--- LeetCode/Database/Second Highest Salary.sql
+++ LeetCode/Database/Second Highest Salary.sql
@@ -1,4 +1,4 @@
-SELECT MAX(SALARY) AS SecondHighestSalary
-FROM Employee SALARY
+SELECT MAX(SALARY) AS SecondHighestSalary  /* SecondHighestSalary = New Table Column Name*/
+FROM Employee
 WHERE SALARY < (SELECT MAX(SALARY)
                 FROM Employee);
