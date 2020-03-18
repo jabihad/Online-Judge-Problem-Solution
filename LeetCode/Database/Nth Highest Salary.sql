489e5fc63e32bd05e00127aff2940cedad50d567
Abu Bakar Siddique <jabihad@gmail.com>
2020-03-18 13:20:25 +0600

    Create Nth Highest Salary.sql

---
+++ LeetCode/Database/Nth Highest Salary.sql
@@ -0,0 +1,13 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  DECLARE M INT;
+  DECLARE column_name varchar(100);
+  set M=N-1;
+  set column_name = CONCAT('getNthHighestSalary', N, ')');
+  RETURN (
+     select DISTINCT Salary as column_name
+     from Employee
+     order by Salary desc
+     LIMIT M, 1
+   );
+END
